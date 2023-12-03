732be5dbfc20b66c14f512fa0bb0086e992e91bf
Ulrich <uhgall@gmail.com>
2023-12-02 16:35:57 -0800

    updated to use raw_text

--- README.md
+++ README.md
@@ -9,6 +9,12 @@ Project for the Tools For Thought Hackathon at AGI House
 If you're coding in Ruby and want to use the glim library:
 https://github.com/uhgall/glim/
 Clone this into a directory called "glim" at the same level as semantic-chunking.
+Then:
+cp .env.sample .env 
+Edit file to put in your API_KEYS
+bundle
+Optionally, test if it's working: (Cost: $0.05 or so)
+bundle exec glim_list_models 
 
 # Modules
 
@@ -16,3 +22,7 @@ Clone this into a directory called "glim" at the same level as semantic-chunking
 Code for using the semantic chunking technique.
 Text file in, text file out.
 
+bundle exec ruby semchunk/make_chunks.rb output/semchunk1
+This reads the first 10 files from raw/ and puts the results in output/semchunk1.
+
+Edit the prompt in chunker1.erb
--- semchunk/make_chunks.rb
+++ semchunk/make_chunks.rb
@@ -1,19 +1,51 @@
 
 require 'glim_ai'
+require 'json'
 
+#  first command line argument is the output directory
+if ARGV.length < 1
+    puts "Usage: ruby make_chunks.rb <output_dir>"
+    exit
+end
 
+@output_dir = ARGV[0]
+Dir.mkdir(@output_dir) unless File.exist?(@output_dir)
 
-def generate_chunks(input)
-    glim = GlimContext.new
-    request = glim.request_from_template('chunker1.erb', input)
-    future = request.send_and_return_future
-    result = future.await_response
-    puts result.completion
+@glim = GlimContext.new
+
+def generate_chunks(input_path)
+    puts ("Processing all files in #{input_path}...")
+
+    file_data = {}
+    Dir.glob(input_path) do |file|
+        item = {}
+        puts ("Processing #{file}...")
+        input = File.read(file)
+        item[:input] = input
+        request = @glim.request_from_template('chunker1', input:)
+        future = request.send_and_return_future
+        item[:future] = future
+        file_data[file] = item
+        break if file_data.length >= 12
+    end
+
+    file_data.each do |file, item|
+        puts ("Waiting for results for #{file}...")
+        future = item[:future]
+        response = future.await_response
+        completion = response.completion
+        json = JSON.parse(completion)
+        json = {file: file, original: item[:input], chunks: json}
+        json_text = JSON.pretty_generate(json)
+        output_path = File.join(@output_dir, File.basename(file, File.extname(file))+".json")
+        puts ("Writing #{output_path}...")
+        File.write(output_path, json_text)
+        puts "Cost so far: $ #{'%5.2f' % @glim.cost}"
+    end
+
 
 end
 
-input = File.read('input.txt')
-
-chunks = generate_chunks(input)
+generate_chunks("raw_text/*.txt")
 
 # puts chunks.join("\n\n")
