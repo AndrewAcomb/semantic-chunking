1cf60f15ac82d1be8e3bf12e99389d3a24045cb8
Michael Vandi <mikevee2013@gmail.com>
2023-12-02 16:09:43 -0800

    Added langdrive completion method

---
+++ eval/langdrive.py
@@ -0,0 +1,41 @@
+import requests
+import json
+
+class LangDrive:
+    def __init__(self) -> None:
+        pass
+
+    def completion(self, prompt: str, model: str, max_tokens: int) -> str:
+        """
+        Make a Post request to call: 
+        POST https://api.langdrive.ai/v1/chat/completions.
+        
+        Returns generated text in response.
+        """     
+        url = "https://api.langdrive.ai/v1/chat/completions"
+        
+        headers = {
+            'Content-Type': 'application/json'
+        }
+        
+        # Prepare the data
+        data = {
+            "prompt": prompt,
+            "model": model,
+            "max_tokens": max_tokens
+        }
+
+        response = requests.post(url, headers=headers, data=json.dumps(data))
+        
+        if response.status_code == 200:
+            # Parse the response & return generated_text
+            # If it was successful or not
+            response_data = json.loads(response.text)
+            
+            if response_data.get('success'):
+                return response_data.get('generated_text', '')
+            else:
+                return "Request unsuccessful."
+                
+        else:
+            return f"HTTP request failed with status code {response.status_code}."
